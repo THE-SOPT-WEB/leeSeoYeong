a61619877f50e592d5109ad71e0c6fc41f337714
leeseooo <leeseoooo@naver.com>
2022-04-14 18:13:56 +0900

    feat: 이미 장바구니에 있는 메뉴 클릭 시 개수만 증가하도록 구현

--- week1/main.js
+++ week1/main.js
@@ -1,23 +1,49 @@
 const $ = (selector) => document.querySelector(selector);
 
-function addMenuInCart(cart, contents) {
-    console.log(contents);
-    const menuBox = cart.querySelector(".menu__box");
-    const menuName = contents[0];
-    const menuPrice = contents[1];
+let cartList = [];
 
-    const menuInfo = document.createElement("ul");
-    menuInfo.classList.add("menu__info-lists");
-    menuInfo.innerHTML = `
+function parsePriceToNumber(price) {
+    const removedComma = price.slice(0, -1)
+        .replace(/\D/g, "");
+
+    return +removedComma;
+};
+
+function addMenuInCart(cart, contents) {
+    const menuBox = cart.querySelector(".menu__box");
+    const totalPrice = cart.querySelector(".cart__container-total > strong");
+
+    const menuName = contents[0];
+    const menuPrice = parsePriceToNumber(contents[1]);
+
+    if (cartList.includes(menuName)) {//이미 장바구니에 넣었다면 개수만 증가
+        const newMenus = document.querySelectorAll(".menu__info-list");
+        for (let newMenu of newMenus) {
+            const newMenuName = newMenu.querySelector(".menu__name").innerText;
+            const newMenuInput = newMenu.querySelector("input");
+            if (newMenuName === menuName) {
+                newMenuInput.value = +newMenuInput.value + 1;
+                console.log(newMenuInput);
+            }
+        }
+        // menuCount.value = +menuCount.value + 1;
+    } else {
+        cartList.push(contents[0]); //장바구니에 메뉴이름 추가
+
+        const menuInfo = document.createElement("ul");
+        menuInfo.classList.add("menu__info-lists");
+        menuInfo.innerHTML = `
         <li class="menu__info-list">
                 <strong class="menu__name">${menuName}</strong> 
                 <input type="number" value="1"/>
-                <strong class="menu__price">${menuPrice}</strong>
+                <strong class="menu__price">${menuPrice}원</strong>
                 <button class="menu__btn-delete">X</button>
             </li>
     `;
-    menuBox.appendChild(menuInfo);
-    console.log(menuBox);
+        menuBox.appendChild(menuInfo);
+    }
+
+    totalPrice.innerText = +totalPrice.innerText + menuPrice;
 }
 
 function clickBurgerCard({ container, cart }) {
