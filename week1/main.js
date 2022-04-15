cab3aebc3ae9e27c54001c890837d33366338594
leeseooo <leeseoooo@naver.com>
2022-04-15 23:01:52 +0900

    feat: 장바구니 취소하기 버튼 클릭 이벤트, X버튼 버그 수정

--- week1/main.js
+++ week1/main.js
@@ -2,18 +2,54 @@ const $ = (selector) => document.querySelector(selector);
 
 let cartList = [];
 
+function showModal(cart) {
+
+}
+
+function resetCart(cart) {
+    const totalPrice = $(".cart__container > .cart__container-total > strong ");
+    const menuBox = cart.querySelector(".menu__box");
+    const menuInfoBox = cart.querySelectorAll("ul");
+    if (menuInfoBox !== undefined) {
+        for (let item of menuInfoBox) {
+            removeItemInCart(item);
+            menuBox.removeChild(item);
+        }
+    }
+    totalPrice.innerText = 0;
+}
+
+function removeItemInCart(menuInfoBox) {
+    let menuName = menuInfoBox.querySelector(".menu__name");
+    let index = cartList.indexOf(menuName);//장바구니에서 해당 메뉴를 찾아서 삭제한다.
+    cartList.splice(index, 1);
+}
+
+function handleCartButtons({ container, cart }) {
+    const buttons = cart.querySelectorAll(".cart__buttons > button");
+    const orderButton = buttons[0];
+    const cancelButton = buttons[1];
+    console.log(cancelButton);
+    orderButton.addEventListener("click", function () {
+        showModal(cart);
+    });
+    cancelButton.addEventListener("click", function () {
+        resetCart(cart);
+    });
+}
+
 function removeMenu(e) {
     const menuName = e.path[1].children[0].innerText;
     const menu = e.path[2]; //지울 메뉴 ul 태그.
     const menuPrice = parsePriceToNumber(e.path[1].children[2].innerText); //지울 메뉴의 가격.
+    const menuCount = e.path[1].children[1].value;
     const menuBox = $(".menu__box");
     const totalPrice = $(".cart__container > .cart__container-total > strong ");
 
     menuBox.removeChild(menu); //.menu__box에서 해당 ul태그를 지운다.
     let index = cartList.indexOf(menuName);//장바구니에서 해당 메뉴를 찾아서 삭제한다.
     cartList.splice(index, 1);
-    totalPrice.innerText = +totalPrice.innerText - menuPrice;
-    console.log(totalPrice);
+    totalPrice.innerText = +totalPrice.innerText - menuPrice * menuCount;
 }
 
 function parsePriceToNumber(price) {
@@ -28,6 +64,7 @@ function addMenuInCart(cart, contents) {
     const totalPrice = cart.querySelector(".cart__container-total > strong");
 
     const menuName = contents[0];
+    const menuCount = 1;
     const menuPrice = parsePriceToNumber(contents[1]);
 
     if (cartList.includes(menuName)) {//이미 장바구니에 넣었던 메뉴라면 개수만 증가
@@ -54,9 +91,9 @@ function addMenuInCart(cart, contents) {
     `;
         menuBox.appendChild(menuInfo);
     }
-    totalPrice.innerText = +totalPrice.innerText + menuPrice; //누적금액을 메뉴가격만큼 증가시킨다.
+    totalPrice.innerText = +totalPrice.innerText + menuPrice * menuCount; //누적금액을 메뉴가격만큼 증가시킨다.
 
-    const removeButtons = document.querySelectorAll(".menu__btn-delete");
+    const removeButtons = document.querySelectorAll(".menu__btn-delete"); // X버튼 클릭 시.
     for (let removeButton of removeButtons) {
         removeButton.addEventListener("click", removeMenu);
     }
@@ -81,6 +118,7 @@ function clickBurgerCard({ container, cart }) {
 }
 
 function storeManager(info) {
+    handleCartButtons(info);
     clickBurgerCard(info); //메뉴 클릭 시 장바구니에 담기.
 }
 
