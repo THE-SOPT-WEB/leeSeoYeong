fcdaf0e2f866e413412c44686ad14b684a8d3d40
leeseooo <leeseoooo@naver.com>
2022-06-13 16:27:26 +0900

    feat: Letter 컴포넌트 스타일링

--- week7/src/components/Letter.tsx
+++ week7/src/components/Letter.tsx
@@ -1,3 +1,26 @@
+import styled from 'styled-components';
+import lock from '../assets/lock.png';
+
 export default function Letter() {
-  return <div>Letter</div>;
+  return (
+    <StWrapper>
+      <StLetter src={lock}></StLetter>
+      <StLetter src={lock}></StLetter>
+      <StLetter src={lock}></StLetter>
+      <StLetter src={lock}></StLetter>
+      <StLetter src={lock}></StLetter>
+      <StLetter src={lock}></StLetter>
+      <StLetter src={lock}></StLetter>
+      <StLetter src={lock}></StLetter>
+    </StWrapper>
+  );
 }
+const StWrapper = styled.div`
+  display: grid;
+  grid-template-columns: repeat(3, 1fr);
+  margin: 20px 0;
+  gap: 20px;
+`;
+const StLetter = styled.img`
+  width: 250px;
+`;
